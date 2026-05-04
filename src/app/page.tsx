42d29be9eee23a59046c9a5d9bab763245200d04
athithyaneonlabz-coder <athithyaneonlabz@gmail.com>
2026-05-04 14:16:54 +0530

    refactor: simplify root page by removing unnecessary login redirection

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,4 @@
 
-'use client'
-
-import { useEffect } from 'react'
-import { useRouter } from 'next/navigation'
-
-export default function Home() {
-  const router = useRouter()
-
-  useEffect(() => {
-    // Redirect to login page
-    router.push('/login')
-  }, [router])
-
-  return null
-}
-
 import { redirect } from 'next/navigation';
 
 export default function RootPage() {
