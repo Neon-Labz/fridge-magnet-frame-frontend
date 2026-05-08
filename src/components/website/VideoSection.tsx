782e39a4a215344b9436a8d8d2766bc2221e7b76
sarmilanneonlabz-SA <sarmilanneonlabz@gmail.com>
2026-05-08 18:02:42 +0530

    adjust video layout alignment for perfect spacing match

--- src/components/website/VideoSection.tsx
+++ src/components/website/VideoSection.tsx
@@ -18,7 +18,7 @@ export default function VideoSection() {
         </div>
 
         {/* Right Video Content */}
-        <div className="flex w-full justify-center lg:w-1/2 lg:justify-end">
+        <div className="flex w-full justify-center lg:w-1/2 lg:justify-start lg:pl-[40px]">
           <div className="w-full max-w-[680px] rounded-[24px] bg-[#F9F9FE] p-[16px] lg:p-[20px]">
             <div className="relative aspect-[4/3] w-full overflow-hidden rounded-[16px]">
               <video
